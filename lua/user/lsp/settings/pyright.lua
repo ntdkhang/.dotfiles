ed9c49fbe45941f9f860a60de326b6f0be7c3c95
ntdkhang <ntdkhang143@gmail.com>
2022-11-15 07:38:41 -0600

    initial commit

---
+++ lua/user/lsp/settings/pyright.lua
@@ -0,0 +1,12 @@
+return {
+	settings = {
+    python = {
+      pythonPath = "/Library/Frameworks/Python.framework/Python",
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "workspace",
+        useLibraryCodeForTypes = true
+      }
+    }
+  }
+}
